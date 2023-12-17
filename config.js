53f542f1cee6a80167fa8b857ec843976e597011
JoschiBot <152638691+JoschiBot@users.noreply.github.com>
2023-12-17 17:15:20 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
+  ['19376882406', 'Pommes, true],
   ['94767939688', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
